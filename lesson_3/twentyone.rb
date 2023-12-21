2d3d5a9322801bf115683a36e6685473cf16c2ff
rtlopez796 <rtlopez796@gmail.com>
2023-12-21 20:18:41 +0000

    Twenty One updates and bug fixes

--- lesson_3/twentyone.rb
+++ lesson_3/twentyone.rb
@@ -3,16 +3,20 @@ PARTITION = "---------------------------------------------------------"
 BUST = 21
 DEALER_MIN = 17
 
-# rubocop:disable Layout/LineLength
 def initialize_deck
-  [
-    ['Hearts', 'Ace'], ['Hearts', '2'], ['Hearts', '3'], ['Hearts', '4'], ['Hearts', '5'], ['Hearts', '6'], ['Hearts', '7'], ['Hearts', '8'], ['Hearts', '9'], ['Hearts', '10'], ['Hearts', 'Jack'], ['Hearts', 'Queen'], ['Hearts', 'King'],
-    ['Diamonds', 'Ace'], ['Diamonds', '2'], ['Diamonds', '3'], ['Diamonds', '4'], ['Diamonds', '5'], ['Diamonds', '6'], ['Diamonds', '7'], ['Diamonds', '8'], ['Diamonds', '9'], ['Diamonds', '10'], ['Diamonds', 'Jack'], ['Diamonds', 'Queen'], ['Diamonds', 'King'],
-    ['Spades', 'Ace'], ['Spades', '2'], ['Spades', '3'], ['Spades', '4'], ['Spades', '5'], ['Spades', '6'], ['Spades', '7'], ['Spades', '8'], ['Spades', '9'], ['Spades', '10'], ['Spades', 'Jack'], ['Spades', 'Queen'], ['Spades', 'King'],
-    ['Clubs', 'Ace'], ['Clubs', '2'], ['Clubs', '3'], ['Clubs', '4'], ['Clubs', '5'], ['Clubs', '6'], ['Clubs', '7'], ['Clubs', '8'], ['Clubs', '9'], ['Clubs', '10'], ['Clubs', 'Jack'], ['Clubs', 'Queen'], ['Clubs', 'King']
-  ]
+  deck = []
+  card_values = [(2..10).to_a, FACE_CARDS, 'Ace'].flatten
+  ['Hearts', 'Diamonds', 'Spades', 'Clubs'].each do |suit|
+    card_values.each do |value|
+      deck << if value.to_i == 0
+                [suit, value]
+              else
+                [suit, value.to_s]
+              end
+    end
+  end
+  deck.shuffle
 end
-# rubocop:enable Layout/LineLength
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -24,45 +28,51 @@ def joinand(arr, delimiter=', ', word='and')
   when 1 then arr.first.to_s
   when 2 then arr.join(" #{word} ")
   else
-    arr[-1] = "#{word} #{arr.last}"
-    arr.join(delimiter)
+    arr[0..-2].join(delimiter) + ", #{word} #{arr.last}"
   end
 end
 
 def deal!(player_hand, dealer_hand, deck)
   2.times do |_|
-    player_hand << deck.sample
-    deck.delete_if { |card| player_hand.include?(card) }
-    dealer_hand << deck.sample
-    deck.delete_if { |card| dealer_hand.include?(card) }
+    player_hand << deck.pop
+    dealer_hand << deck.pop
   end
 end
 
+def hit!(deck, hand)
+  hand << deck.pop
+end
+
 def player_turn(player_hand, dealer_hand, deck)
-  display_hands(player_hand, dealer_hand)
+  player_total = total(player_hand)
+  display_hands(player_hand, player_total, dealer_hand)
   loop do
     prompt "Would you like to hit or stay?"
     choice = gets.chomp.downcase
     if choice.start_with?('h')
       hit!(deck, player_hand)
-      prompt "You drew a #{player_hand[-1][1]} for a total of #{total(player_hand)}."
+      player_total = total(player_hand)
+      prompt "You drew a #{player_hand[-1][1]} of #{player_hand[-1][0]} " \
+             "for a total of #{player_total}."
     end
-    break if choice.start_with?('s') || busted?(player_hand)
+    break if choice.start_with?('s') || busted?(player_total)
   end
-  prompt "Bust! Dealer wins" if busted?(player_hand)
+  prompt "Bust! Dealer wins" if busted?(player_total)
 end
 
 def dealer_turn(dealer_hand, deck)
+  dealer_total = total(dealer_hand)
   loop do
-    break if total(dealer_hand) > DEALER_MIN || busted?(dealer_hand)
+    break if dealer_total > DEALER_MIN || busted?(dealer_total)
     hit!(deck, dealer_hand)
+    dealer_total = total(dealer_hand)
+    prompt "Dealer drew a #{dealer_hand.last[1]} of #{dealer_hand.last[0]}."
   end
-  if busted?(dealer_hand)
+  if busted?(dealer_total)
     prompt "Dealer busts. You win!"
   else
     prompt "Dealer stays."
   end
-  prompt PARTITION
 end
 
 def total(hand)
@@ -97,14 +107,8 @@ def hand_values(hand)
   hand.map { |card| card[1] }
 end
 
-def hit!(deck, hand)
-  card = deck.sample
-  deck.delete(card)
-  hand << card
-end
-
-def busted?(hand)
-  total(hand) > BUST
+def busted?(total)
+  total > BUST
 end
 
 def detect_winner(player_hand, dealer_hand)
@@ -127,16 +131,26 @@ def play_again?
   answer.start_with?('y')
 end
 
-def display_hands(player_hand, dealer_hand)
-  prompt "Dealer has: #{dealer_hand[0][1]} and 1 unknown card"
-  player_values = hand_values(player_hand)
-  player_total = total(player_hand)
-  prompt "Player has: #{joinand(player_values)} for a total of #{player_total}"
+def display_hands(player_hand, player_total, dealer_hand)
+  prompt "Dealer has: #{dealer_hand[0][1]} " \
+         "of #{dealer_hand[0][0]} and 1 unknown card"
+  prompt "Player has: #{joinand(format_hand(player_hand))} " \
+         "for a total of #{player_total}"
+end
+
+def format_hand(hand)
+  hand.map do |card|
+    "#{card[1]} of #{card[0]}"
+  end
 end
 
 def display_totals(player_hand, dealer_hand)
-  prompt "Player total: #{total(player_hand)}"
-  prompt "Dealer total: #{total(dealer_hand)}"
+  player_values = hand_values(player_hand)
+  prompt "Player had #{joinand(format_hand(player_hand))} " \
+         "for a total of #{total(player_hand)}"
+  dealer_values = hand_values(dealer_hand)
+  prompt "Dealer had #{joinand(format_hand(dealer_hand))} " \
+         "for a total of #{total(dealer_hand)}"
 end
 
 def display_wins(player_wins, dealer_wins)
@@ -149,6 +163,17 @@ loop do
   player_wins = 0
   dealer_wins = 0
   loop do
+    display_wins(player_wins, dealer_wins)
+    prompt PARTITION
+
+    if player_wins == 5
+      prompt "Game over! Player wins."
+      break
+    elsif dealer_wins == 5
+      prompt "Game over! Dealer wins."
+      break
+    end
+    
     deck = initialize_deck
     player_hand = []
     dealer_hand = []
@@ -156,33 +181,39 @@ loop do
     deal!(player_hand, dealer_hand, deck)
 
     player_turn(player_hand, dealer_hand, deck)
-    dealer_wins += 1 if busted?(player_hand)
-    prompt PARTITION
-    dealer_turn(player_hand, dealer_hand, deck) if !busted?(player_hand)
-    player_wins += 1 if busted?(dealer_hand)
-    display_totals(player_hand, dealer_hand)
-    prompt PARTITION
-    if !busted?(player_hand) && !busted?(dealer_hand)
-      if winner?(player_hand, dealer_hand)
-        prompt "#{detect_winner(player_hand, dealer_hand)} wins!"
+    player_total = total(player_hand)
+    dealer_total = total(dealer_hand)
+    if busted?(player_total)
+      dealer_wins += 1
+      display_totals(player_hand, dealer_hand)
+      prompt PARTITION
+      next
+    else
+      dealer_turn(dealer_hand, deck)
+      dealer_total = total(dealer_hand)
+      if busted?(dealer_total)
+        player_wins += 1
+        display_totals(player_hand, dealer_hand)
         prompt PARTITION
-        if detect_winner(player_hand, dealer_hand) == 'Player'
-          player_wins += 1
-        else
-          dealer_wins += 1
-        end
-      else
-        prompt "It's a tie!"
-        prompt PARTITION
+        next
       end
     end
-    display_wins(player_wins, dealer_wins)
-    prompt PARTITION
 
-    if player_wins == 5
-      break prompt "Game over! Player wins."
-    elsif dealer_wins == 5
-      break prompt "Game over! Dealer wins."
+    case
+    when player_total > dealer_total
+      player_wins += 1
+      prompt "Player wins!"
+      display_totals(player_hand, dealer_hand)
+      prompt PARTITION
+    when player_total < dealer_total
+      dealer_wins += 1
+      prompt "Dealer wins!"
+      display_totals(player_hand, dealer_hand)
+      prompt PARTITION
+    else
+      prompt "It's a tie!"
+      display_totals(player_hand, dealer_hand)
+      prompt PARTITION
     end
   end
   break unless play_again?
